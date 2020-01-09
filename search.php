8d7b77ebeee1a26751214659823969a79359d0df
Prashant Bajak <bajakprashant1798@gmail.com>
2020-01-09 18:33:45 +0530

    trying to add WordPress coding standards

--- search.php
+++ search.php
@@ -1,24 +1,14 @@
 <?php get_header(); ?>
-
-    <?php
-
-        if( have_posts() ){
-            ?>
-
+    <?php if (have_posts()) { ?>
             <h2>Search results for: <?php the_search_query(); ?></h2>
-
-            <?php
-
-            while( have_posts() ){   
+        <?php while (have_posts()){   
                 the_post();    
                 get_template_part('content'); 
-            }
-            
-            echo paginate_links();
-                }
-        else{
-            echo '<p>No content found</p>';
-        }
+        }            
+        echo paginate_links();
+    }
+    else{
+        echo '<p>No content found</p>';
+    }
     ?>
-    
 <?php get_footer(); ?>
