78cfba90d3e6ddc051b229156cce6e6f1ba6f7d3
Prashant Bajak <bajakprashant1798@gmail.com>
2020-01-09 18:33:00 +0530

    trying to add WordPress coding standards

--- portfolio.php
+++ portfolio.php
@@ -8,23 +8,21 @@
 <div class="container">
     <div class="container-portfolio">
         <div class="row">
-    <?php
-        if( have_posts() ) :
-            while ( have_posts() ) : the_post(); ?>
-        
-        
+    <?php if(have_posts()) : 
+        while(have_posts()) : the_post(); ?>
         <div class="col-lg-4 col-sm-6 portfolio-item">
             <div class="card h-100">
                 <a href="<?php the_permalink(); ?>"><?php  the_post_thumbnail(); ?></a>      
             </div>
         </div>
-
-
             <?php previous_posts_link() ?>
-            <?php next_posts_link('<img src="'.get_template_directory_uri().'/img/pagination-arrow.png" />' ); ?>
-        <?php  endwhile;
-        endif;
-        ?>
+            <?php 
+                next_posts_link('<img src="'.get_template_directory_uri().'/img/pagination-arrow.png" />' ); 
+            ?>
+                    <?php  
+        endwhile;
+    endif;
+    ?>
         </div>
     </div>
 </div>
