62a1ef58a4616686009eb0559a1c5ea76ddfcdc0
Prashant Bajak <bajakprashant1798@gmail.com>
2020-01-09 18:30:11 +0530

    trying to add WordPress coding standards

--- index.php
+++ index.php
@@ -1,34 +1,22 @@
 <?php get_header(); ?>
-<div class="container">
-    
+<div class="container">    
     <div class="site-content clearfix"> <!-- site-content -->
-    
         <div class="main-column"> <!-- main-column -->
-
-            <?php
-
-            if( have_posts() ){
-
-                while( have_posts() )
-                { the_post();
-                    
-                        
-            get_template_part('content', get_post_format());                        
-
-            }
-            ?>
+            <?php if (have_posts()) {
+                while (have_posts()) { the_post();
+                    get_template_part('content', get_post_format());
+                } ?>
        
                 <?php previous_posts_link() ?>
-                <?php next_posts_link('<img src="'.get_template_directory_uri().'/img/pagination-arrow.png" />' ); ?>
+                <?php 
+                    next_posts_link('<img src="'.get_template_directory_uri().'/img/pagination-arrow.png" />' ); 
+                ?>
         
-            <?php
-                }
-            else{
+            <?php }
+            else {
                 echo '<p>No content found</p>';
-            }
-            ?>
-        </div><!-- /main-column -->
-
+            } ?>
+    </div><!-- /main-column -->
         <!-- secondry column -->
         <div class="secondary-column">
             <?php dynamic_sidebar('sidebar1'); ?>
