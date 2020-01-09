d7b3b5430c998dc84944419865240b2d1af401cf
Prashant Bajak <bajakprashant1798@gmail.com>
2020-01-09 18:34:28 +0530

    trying to add WordPress coding standards

--- searchform.php
+++ searchform.php
@@ -1,6 +1,8 @@
 <form action="/" method="get">
-    <label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
-        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+    <label class="screen-reader-text" for="s">
+        <?php _x('Search for:', 'label'); ?>
+    </label>
+        <input type="text" value="<?php echo get_search_query(); ?> " name="s" id="s" />
         <input type="submit" id="searchsubmit"
-            value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+            value="<?php echo esc_attr_x('Search', 'submit button'); ?>" />
 </form>
